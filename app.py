ee7c9641ab9f3923079876f1c4d0cec294adb915
Renuka Khirwadkar <renuka.khirwadkarr@gmail.com>
2026-03-02 22:26:50 +0530

    updated gemini model

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if api_key:
         else:
             with st.spinner("Generating script with Gemini..."):
                 try:
-                    model = genai.GenerativeModel("gemini-1.5-flash")
+                    model = genai.GenerativeModel("gemini-1.5-flash-001")
 
                     if "QMate" in test_type:
                         prompt = f"""
